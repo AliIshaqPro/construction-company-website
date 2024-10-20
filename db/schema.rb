ae0333462b2864660ae0f3c578e977a82034c70f
Ali Ishaq <aliishaqsandho.com>
2024-10-20 10:48:01 +0500

    bug fixed of margin

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_032619) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
+  end 
 
   create_table "sliders", force: :cascade do |t|
     t.string "title"
