631dfd674f6aea0b3547c94364a87048a05c93a2
Ali Ishaq <aliishaqsandho.com>
2024-11-18 05:44:25 +0500

    s

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,25 @@
 class ReviewsController < ApplicationController
+    # GET /reviews
+    def new
+      @review = Review.new
+    end
+  
+    # POST /reviews
     def create
       @review = Review.new(review_params)
-      if @review.save
-        redirect_to reviews_path, notice: 'Review submitted successfully!'
+  
+      # Check if the entered PIN code matches the predefined PIN code
+      if params[:review][:pin_code] == '2269'
+        # Save the review if the pin code is correct
+        if @review.save
+          redirect_to reviews_path, notice: 'Review submitted successfully.'
+        else
+          redirect_to reviews_path, alert: 'There was an error submitting your review.'
+        end
       else
-        redirect_to reviews_path, alert: 'Failed to submit review.'
+        # If the PIN code is incorrect, show a flash message
+        flash[:alert] = "Incorrect Pin Code. You are not authorized to give a review. Please contact us for access."
+        redirect_to root_path
       end
     end
   
